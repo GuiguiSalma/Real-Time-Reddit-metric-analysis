df23f6beff71c96bfae29845e94a8f561d276f2b
GUIGUI Salma <salma-guigui@outlook.fr>
2024-10-09 04:11:07 +0100

    adding them senitive files to gitignore

--- backend/data_ingestion/praw_config.py
+++ backend/data_ingestion/praw_config.py
@@ -2,8 +2,8 @@ import praw
 
 def get_reddit_client():
     reddit = praw.Reddit(
-        client_id='no',
-        client_secret='non',
-        user_agent='LA27'
+        client_id='REMOVED',
+        client_secret='U AIN'T GETTING MY KEY',
+        user_agent='Metric Analysis'
     )
     return reddit
